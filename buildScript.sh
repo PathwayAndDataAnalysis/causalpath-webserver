6842b08e107d776b5e0f5f89008453d7854ebd84
fdurupinar <f.durupinar@gmail.com>
2018-01-30 13:46:29 -0800

    small updates

---
+++ buildScript.sh
@@ -0,0 +1,14 @@
+
+cd jar
+cd causalpath
+sudo git pull origin master
+sudo mvn clean install
+sudo mvn assembly:single
+cp ./target/causalpath.jar ../causalpath.jar
+cd ..
+cd ..
+sudo mkdir analysisOut
+cd analysisOut
+sudo mkdir demo
+cd ..
+
--- jar/buildScript.sh
+++
@@ -1,6 +0,0 @@
-git pull
-cd causalpath
-mvn clean install
-mvn assembly:single
-cp ./target/causalpath.jar ../causalpath.jar
-cd ..
